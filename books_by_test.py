af4447d477d916c13df2b98d19beda17d23d18f1
Victor 7004 <turist70@i.ua>
2021-01-16 19:32:00 +0200

    add test

---
+++ books_by_test.py
@@ -0,0 +1,27 @@
+import pytest
+from selenium import webdriver
+from selenium.common.exceptions import NoAlertPresentException
+import math
+import time
+
+
+def test_guest_should_see_login_link(browser):
+    link = f"http://selenium1py.pythonanywhere.com/catalogue/the-shellcodershandbook_209/?promo=newYear" 
+    browser.get(link)
+    time.sleep(30)
+    button = browser.find_element_by_class_name (".btn-add-to-basket") 
+    button.click() 
+
+def solve_quiz_and_get_code(self):
+    alert = self.browser.switch_to.alert
+    x = alert.text.split(" ")[2]
+    answer = str(math.log(abs((12 * math.sin(float(x))))))
+    alert.send_keys(answer)
+    alert.accept()
+    try:
+        alert = self.browser.switch_to.alert
+        alert_text = alert.text
+        print(f"Your code: {alert_text}")
+        alert.accept()
+    except NoAlertPresentException:
+        print("No second alert presented")

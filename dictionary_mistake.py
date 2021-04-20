dc187cb6f549587749643eba1ed77bb8c9bffde1
Victor7004 <viktoraleksejcuk@gmail.com>
2021-04-20 20:17:05 +0300

    add file

---
+++ dictionary_mistake.py
@@ -0,0 +1,12 @@
+dictionary = set()
+dictionary_lower = set()
+for _ in range(int(input())):
+    word = input()
+    dictionary.add(word)
+    dictionary_lower.add(word.lower())
+sentence = input().split()
+mistake = 0
+for word in sentence:
+    if (word not in dictionary and word.lower() in dictionary_lower) or (word not in dictionary and word == word.lower()) or len([letter for letter in word if letter == letter.upper()]) > 1:
+        mistake += 1
+print(mistake)   

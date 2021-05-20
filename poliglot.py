4b73c670e548268a0f9fdfdd6a5d791bfa26962b
Victor7004 <viktoraleksejcuk@gmail.com>
2021-05-20 15:01:52 +0300

    Update poliglot.py

--- poliglot.py
+++ poliglot.py
@@ -1,3 +1,4 @@
+#простейшая пайтон программа на сортировку множеств
 students = [{input() for j in range(int(input()))} for i in range(int(input()))]
 known_by_everyone, known_by_someone = set.intersection(*students), set.union(*students)
 print(len(known_by_everyone), *sorted(known_by_everyone), sep='\n')

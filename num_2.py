61dc52f1f9608bc902338cb2fd8dcc7e4f2042bc
Victor7004 <viktoraleksejcuk@gmail.com>
2021-06-11 20:47:49 +0300

    Update num_2.py

--- num_2.py
+++ num_2.py
@@ -3,3 +3,7 @@ print(a // 10)
 #принимаем на входе два целых числа и выводим их сумму
 a, b = int(input()), int(input())
 print(a + b)
+#выведение последней цифры числа
+# put your python code here
+a = int(input())
+print((a % 10))

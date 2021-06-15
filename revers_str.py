a9a9b89754e380a681b989e5a005b9727b449220
Victor7004 <viktoraleksejcuk@gmail.com>
2021-06-15 20:36:57 +0300

    Create revers_str.py

---
+++ revers_str.py
@@ -0,0 +1,12 @@
+#функция которая переварачивает строку наоборот
+const reverse = (str) => {
+  let i = (str.length-1);
+  let result = '';
+
+  while (i >= 0) {
+    result = result + str[i];
+    i = i - 1;
+  }
+
+  return result;
+}

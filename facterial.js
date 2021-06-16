0179f9f6d0023e26ef591ab78db924a29cfb3e78
Victor7004 <viktoraleksejcuk@gmail.com>
2021-06-16 19:50:28 +0300

    Create facterial.js

---
+++ facterial.js
@@ -0,0 +1,10 @@
+const factorial = (n) => {
+  let result  = 1;
+
+  // initialization↓    condition↓     update↓
+  for (let counter = 1; counter <= n; counter++) {
+    result *= counter;
+  }
+
+  return result;
+}

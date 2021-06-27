46279ace367c5bd515e280fd279e3bc4ce1e62cc
Victor7004 <viktoraleksejcuk@gmail.com>
2021-06-27 19:18:33 +0300

    Update JS_2.js

--- JS_2.js
+++ JS_2.js
@@ -1,7 +1,7 @@
 //функция находящая наименьший делитель
 const smallestDivisor = (num) => { //создаем функцию с названием
   const iter = (counter, acc) => { // создаем новую переменную iter
-    if (counter === 1) {
+    if (counter === 1) { // прописываем первое условие 
       return 1;
     }
       if (counter%acc===0) {
